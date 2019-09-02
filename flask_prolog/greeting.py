ae7f3081ea5d847ea4998a30dbf8339c1a4c5f81
Paul Brown <pbrownpaul@gmail.com>
2019-09-02 19:27:06 +0100

    Make pengines for multi-threaded support

--- flask_prolog/greeting.py
+++ flask_prolog/greeting.py
@@ -8,36 +8,42 @@ class Greetings:
     """
 
     def __init__(self):
-        pengine_builder = PengineBuilder(urlserver="http://localhost:5050",
-                                         application="greet",
-                                         destroy=False)
-        self.pengine = Pengine(builder=pengine_builder)
+        self.pengine_builder = PengineBuilder(urlserver="http://localhost:5050",
+                                         application="greet")
+
+    def new_pengine(self) :
+            return Pengine(builder=self.pengine_builder)
 
     def add_salutation(self, salutation):
-        query = self.pengine.ask(f"add_salutation('{salutation}')")
-        self.pengine.doAsk(query)
-        self.pengine.iAmFinished(query)
+        pengine = self.new_pengine()
+        query = pengine.ask(f"add_salutation('{salutation}')")
+        pengine.doAsk(query)
+        pengine.iAmFinished(query)
 
     def add_adjective(self, adjective):
-        query = self.pengine.ask(f"add_adjective('{adjective}')")
-        self.pengine.doAsk(query)
-        self.pengine.iAmFinished(query)
+        pengine = self.new_pengine()
+        query = pengine.ask(f"add_adjective('{adjective}')")
+        pengine.doAsk(query)
+        pengine.iAmFinished(query)
 
     def remove_salutation(self, salutation):
-        query = self.pengine.ask(f"remove_salutation('{salutation}')")
-        self.pengine.doAsk(query)
-        self.pengine.iAmFinished(query)
+        pengine = self.new_pengine()
+        query = pengine.ask(f"remove_salutation('{salutation}')")
+        pengine.doAsk(query)
+        pengine.iAmFinished(query)
 
     def remove_adjective(self, adjective):
-        query = self.pengine.ask(f"remove_adjective('{adjective}')")
-        self.pengine.doAsk(query)
-        self.pengine.iAmFinished(query)
+        pengine = self.new_pengine()
+        query = pengine.ask(f"remove_adjective('{adjective}')")
+        pengine.doAsk(query)
+        pengine.iAmFinished(query)
 
     def greeting(self, name):
-        query = self.pengine.ask(f"random_greeting('{name.title()}', Greet)")
-        self.pengine.doAsk(query)
-        greet = self.pengine.currentQuery.availProofs[0]['Greet']
-        self.pengine.iAmFinished(query)
+        pengine = self.new_pengine()
+        query = pengine.ask(f"random_greeting('{name.title()}', Greet)")
+        pengine.doAsk(query)
+        greet = pengine.currentQuery.availProofs[0]['Greet']
+        pengine.iAmFinished(query)
         return greet
 
 
